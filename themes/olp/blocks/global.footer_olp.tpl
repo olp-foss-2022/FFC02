53095986269a87c88790610bc8929bd308843b0c
uc ubuntu <nguyenuc09112000@gmail.com>
2022-03-24 12:46:45 +0700

    tạo các block mới

---
+++ themes/olp/blocks/global.footer_olp.tpl
@@ -0,0 +1,21 @@
+<!-- BEGIN: main -->
+<style>
+    .content-footer {
+        text-align: center !important;
+    }
+    .name-page {
+        font-size: 20px;
+        font-weight: bold;
+    }
+    hr {
+        color: black;
+    }
+</style>
+<hr>
+<div class="content-footer">
+    <p class="name-page">{GLANG.name_page}</p>
+    <p>{GLANG.title_description_page}: {GLANG.description_page}</p>
+    <p>{GLANG.title_contact_head}: {GLANG.contact_head} - {GLANG.title_contact_tel}: {GLANG.contact_tel} - {GLANG.title_contact_email}: {GLANG.contact_email} - {GLANG.title_contact_web}: {GLANG.contact_web}</p>
+    
+</div>
+<!-- END: main -->
---
+++ themes/olp/blocks/global.logotitle.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * NukeViet Content Management System
+ * @version 4.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2021 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+
+if (!defined('NV_MAINFILE')) {
+    exit('Stop!!!');
+}
+
+if (!nv_function_exists('nv_menu_theme_logotitle')) {
+    function nv_menu_theme_logotitle($block_config)
+    {
+        global $global_config, $site_mods, $lang_global;
+        if (file_exists(NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/blocks/global.logotitle.tpl')) {
+            $block_theme = $global_config['module_theme'];
+        } elseif (file_exists(NV_ROOTDIR . '/themes/' . $global_config['site_theme'] . '/blocks/global.logotitle.tpl')) {
+            $block_theme = $global_config['site_theme'];
+        } else {
+            $block_theme = 'default';
+        }
+        $xtpl = new XTemplate('global.logotitle.tpl', NV_ROOTDIR . '/themes/' . $block_theme . '/blocks');
+        $xtpl->assign('GLANG', $lang_global);
+        $xtpl->assign('URLSITE', NV_BASE_SITEURL);
+        $xtpl->assign('IMG_LOGO', NV_BASE_SITEURL.'/assets/images/logo_olp.jpeg');
+
+
+        $xtpl->parse('main');
+        return $xtpl->text();
+    }
+}
+
+if (defined('NV_SYSTEM')) {
+    $content = nv_menu_theme_logotitle($block_config);
+}
